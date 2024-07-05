f49f626515156f9caeab9362dcae130d7a017ae5
arunv1shwa <arunvishwa.temp@gmail.com>
2024-07-05 09:37:45 +0530

    v 1.1 - preview

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'c1': "url('../assets/img/c1.jpg')",
+        'campus0': "url('/assets/img/campus0.jpg')",
+        'campus1': "url('/assets/img/campus1.jpg')",
+        'campus2': "url('/assets/img/campus2.jpg')",
+        'campus3': "url('/assets/img/campus3.jpg')",
+      },
+      animation: {
+        'move-around': 'move-around 5s ease-in-out infinite',
+        'scroll':'scroll 30s linear infinite reverse running',
+        'scroll-pause':'scroll pause'
+      },
+      keyframes: {
+        'move-around': {
+          '0%': { 'background-position':'0 50%' },
+          '50%': { 'background-position':'100% 50%' },
+          '100%': { 'background-position':'0 50%' }
+        },
+        'scroll':{
+          '0%': { 'transform':'translateX(-100%)' },
+          '100%': { 'transform':'translateX(160%)' }
+        }
       }
     },
   },
